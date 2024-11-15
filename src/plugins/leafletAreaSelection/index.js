e92fb01d6f5707bb957dfabfeab3b13fb64e7bba
zczcyc0201 <zczcyc0201@126.coom>
2024-11-15 11:43:11 +0800

    3.5.3

--- src/plugins/leafletAreaSelection/index.js
+++
@@ -1,9 +0,0 @@
-import { Util, Control } from 'leaflet';
-import { DrawAreaSelection, drawAreaSelection } from './control';
-
-Util.extend(Control, {
-  DrawAreaSelection,
-  drawAreaSelection,
-});
-
-export { DrawAreaSelection, drawAreaSelection };
--- src/plugins/leafletAreaSelection/tests/utils.test.js
+++
@@ -1,7 +0,0 @@
-import { toCamelCase } from '../utils';
-
-describe('utils', () => {
-  it('should compose a camel-case class name', () => {
-    expect(toCamelCase('foo-bar-baz')).toBe('fooBarBaz');
-  });
-});
